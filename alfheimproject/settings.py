2aca6c3d25e7cf3c51d9e18d30c2955bba36400c
kubixservice <inspectahdeck227@gmail.com>
2018-08-10 11:56:22 +0300

    - SECRET_KEY fix

--- alfheimproject/settings.py
+++ alfheimproject/settings.py
@@ -18,7 +18,7 @@ from .local_settings import *
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '^y)jr_dxvg!w(y0af6!+6lc==hhy+9^zso8=e)%ijpra-+eij!'
+SECRET_KEY = SECRETS['secret_key']
 
 # Application definition
 
