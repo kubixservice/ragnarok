edbfd5da63ceee87a1df8c6bd4cc3e9252921bde
kubixservice <inspectahdeck227@gmail.com>
2018-08-18 16:52:35 +0300

    - TravisCI update config

---
+++ travis.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+echo "
+{
+  "secret_key": "",
+  "db_engine": "",
+  "db_host": "",
+  "db_username": "",
+  "db_password": "",
+  "db_database": "",
+  "db_port": 0,
+  "table_prefix": "cp_",
+  "smtp": {
+    "email_host": "",
+    "email_host_user": "",
+    "email_port": 0,
+    "email_use_ssl": true,
+    "email_host_password": ""
+  }
+}
+" > alfheimproject/conf/secrets.json
